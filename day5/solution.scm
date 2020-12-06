9c080a00521dae5aa945979e1af36b97d6263e84
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-06 23:47:25 +0200

    Solution for day 5

--- day5/solution.scm
+++ day5/solution.scm
@@ -1,7 +1,81 @@
-;var rowBinary = "FBFBBFF".replace(/F/g, "0").replace(/B/g, "1")
-;parseInt(rowBinary, 2) -- row number
+(define input-data "
+FFFBBFBLLR
+BFBBBFFRLR
+BFBBBBFLRR
+")
 
-;var columnBinary = "RLR".replace(/R/g, "1").replace(/L/g, "0")
-;parseInt(columnBinary, 2) -- column number
+; Utility functions
+(define (split-list-by l el)
+  (define (split l el splitted-part already-splitted)
+    (cond ((null? l)
+            (cons (reverse splitted-part) already-splitted))
+          ((eq? (car l) el)
+            (split (cdr l) el '() (cons (reverse splitted-part) already-splitted)))
+          (else
+            (split (cdr l) el (cons (car l) splitted-part) already-splitted))))
+  (reverse (split l el '() '())))
 
-; (string->number "1010" 2) -- can accept the radix parameter
+; Parser
+(define (parse-passes input)
+  (let ((passes-inputs
+          (split-list-by input '#\newline)))
+    (map
+      parse-pass
+      (filter
+        (lambda (p) (> (length p) 0))
+        passes-inputs))))
+
+(define (parse-pass input)
+  (define (list-of-zero-ones->number l)
+    (string->number
+      (list->string l)
+      2))
+  (define (replace-character ch)
+    (cond ((equal? ch '#\B) '#\1)
+          ((equal? ch '#\F) '#\0)
+          ((equal? ch '#\R) '#\1)
+          ((equal? ch '#\L) '#\0)
+          (else ch)))
+  (define (replace-characters input output)
+    (if (null? input)
+      (list-of-zero-ones->number
+        (reverse output))
+      (replace-characters
+        (cdr input)
+        (cons
+          (replace-character (car input))
+          output))))
+  (replace-characters input '())
+)
+
+; Finding the gap in the scanned passes
+(define (find-missing-pass passes)
+  (define (has-one-element-gap pair)
+    (equal? (cadr pair) (+ 2 (car pair))))
+  (let ((sorted-passes
+           (sort passes (lambda (x y) (< x y)))))
+    (let ((pairs
+             (zip sorted-passes (cdr sorted-passes))))
+      (let ((fitting-pairs
+               (filter has-one-element-gap pairs)))
+        (if (null? fitting-pairs) (error "Did not find a gap for a pass")
+          (+ (car (car fitting-pairs)) 1))))))
+
+; Solution
+
+(define passes
+  (parse-passes
+    (string->list input-data)))
+
+(newline)
+(display "Part 1:")
+(newline)
+(display
+  (apply max passes))
+(newline)
+
+(newline)
+(display "Part 2:")
+(newline)
+(display
+  (find-missing-pass passes))
