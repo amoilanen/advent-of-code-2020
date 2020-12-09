d0699632fcdbd654703bb9128613f777749b1a11
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-10 00:15:08 +0200

    Solution for part 2 of day 9

--- day9/solution.scm
+++ day9/solution.scm
@@ -20,6 +20,7 @@
 309
 576
 ")
+(define preamble-size 5)
 
 ; Utility functions
 (define (contains? el list)
@@ -37,6 +38,18 @@
 
 (define (split-list-by l el)
   (split-list-by-list l (list el)))
+
+(define (drop-from-tail l num)
+  (reverse
+    (drop
+      (reverse l)
+      num)))
+
+(define (sublist from to l)
+  (let ((len (length l)))
+    (drop-from-tail
+      (drop l from)
+      (- len 1 to))))
 
 (define (unique-n-tuples-of elements n)
   (define (element-pairs first rest)
@@ -88,13 +101,54 @@
           current-number))))
   (find-number (drop xmas-numbers preamble-size) (take xmas-numbers preamble-size)))
 
+(define (find-range-summing-to-invalid-xmas-number xmas-numbers invalid-number)
+  (define xmas-numbers-length (length xmas-numbers))
+  (define (is-desired-range? range)
+    (equal? (apply + range) invalid-number))
+  (define (find-range-starting-at-index range-start-index)
+    (define (find-range-of-length-greater-or-equal range-length)
+      (let ((range-end-index (+ range-start-index range-length)))
+        (if (>= range-end-index xmas-numbers-length) #f
+          (let ((range (sublist range-start-index range-end-index xmas-numbers)))
+            (if (is-desired-range? range) range
+              (find-range-of-length-greater-or-equal (+ 1 range-length)))))))
+    (let ((found-range-at-start-index (find-range-of-length-greater-or-equal 2)))
+      (if found-range-at-start-index found-range-at-start-index
+        (let ((next-start-index (+ 1 range-start-index)))
+          (if (>= next-start-index xmas-numbers-length) #f
+            (find-range-starting-at-index next-start-index))))))
+    (find-range-starting-at-index 0))
+
+(define (sum-of-max-and-min l)
+  (let ((min-number (apply min l))
+        (max-number (apply max l)))
+    (+ min-number max-number)))
+
+(define (find-weakness xmas-numbers invalid-number)
+  (let ((weakness-range (find-range-summing-to-invalid-xmas-number xmas-numbers invalid-number)))
+    (if weakness-range
+      (sum-of-max-and-min weakness-range)
+      #f)))
+
+(define parsed-numbers
+  (parse-numbers
+    (string->list input-data)))
 
 (newline)
 (display "Part 1:")
 (newline)
-(display
+(define first-invalid-number
   (find-first-invalid-xmas-number
-    (parse-numbers
-      (string->list input-data))
-    5))
+    parsed-numbers
+    preamble-size))
+(display first-invalid-number)
+(newline)
+
+(newline)
+(display "Part 2:")
+(newline)
+(display
+  (find-weakness
+    parsed-numbers
+    first-invalid-number))
 (newline)
