dab1f3a531bf0a9f07bc49b8a1104fc6be2b64a6
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-11 01:00:30 +0200

    Inefficient part 2 of day 10

--- day10/solution.scm
+++ day10/solution.scm
@@ -1,15 +1,107 @@
 (define input-data "
-16
-10
-15
-5
-1
-11
-7
-19
-6
-12
+149
+87
+67
+45
+76
+29
+107
+88
 4
+11
+118
+160
+20
+115
+130
+91
+144
+152
+33
+94
+53
+148
+138
+47
+104
+121
+112
+116
+99
+105
+34
+14
+44
+137
+52
+2
+65
+141
+140
+86
+84
+81
+124
+62
+15
+68
+147
+27
+106
+28
+69
+163
+97
+111
+162
+17
+159
+122
+156
+127
+46
+35
+128
+123
+48
+38
+129
+161
+3
+24
+60
+58
+155
+22
+55
+75
+16
+8
+78
+134
+30
+61
+72
+54
+41
+1
+59
+101
+10
+85
+139
+9
+98
+21
+108
+117
+131
+66
+23
+77
+7
+100
+51
 ")
 
 (define max-jolt-difference 3)
@@ -36,6 +128,13 @@
     l
     (take l n)))
 
+(define (drop-until p l)
+  (cond ((null? l) '())
+        ((p (car l)) l)
+        (else (drop-until
+                p
+                (cdr l)))))
+
 ; Parser
 (define (parse-numbers input)
   (let ((numbers-input (split-list-by input '#\newline)))
@@ -45,6 +144,7 @@
         (lambda (x) (> (length x) 0))
         numbers-input))))
 
+; Solution
 (define (sorted-full-chain adapters)
   (let ((max-adapter (apply max adapters)))
     (let ((device (+ max-jolt-difference max-adapter)))
@@ -88,7 +188,7 @@
           (let ((connectable-adapters
             (filter
               (lambda (a)
-                (<= (- a current-adapter) 3))
+                (<= (- a current-adapter) max-jolt-difference))
               (take-at-most (cdr remaining-adapters) max-jolt-difference))))
             (process-next-adapter
               (cdr remaining-adapters)
@@ -96,6 +196,31 @@
                 connectivity-list
                 (list (list current-adapter connectable-adapters))))))))
     (process-next-adapter full-chain '())))
+
+; Inefficient recursive algorithm: fails with "out of memory"! Come up with a more efficient version
+(define (adapter-combinations connectivity-list)
+  (if (null? connectivity-list) '()
+    (let ((first-adapter-connectivity (car connectivity-list))
+          (rest-of-connectivity-list (cdr connectivity-list)))
+      (let ((adapters-connectable-to-first-adapter (cadr first-adapter-connectivity))
+            (first-adapter (car first-adapter-connectivity)))
+        (let ((next-adapter-combinations
+                 (apply
+                   append
+                   (map
+                     (lambda (next-adapter)
+                       (let ((next-connectivity-list
+                                (drop-until
+                                   (lambda (connectivity)
+                                     (equal? (car connectivity) next-adapter))
+                                   rest-of-connectivity-list)))
+                         (adapter-combinations next-connectivity-list)))
+                     adapters-connectable-to-first-adapter))))
+          (if (null? next-adapter-combinations) (list first-adapter)
+            (map
+              (lambda (c)
+                (cons first-adapter c))
+              next-adapter-combinations)))))))
 
 ; Output
 
@@ -117,6 +242,8 @@
 (display "Part 2:")
 (newline)
 (display
-  (construct-connectivity-list
-    adapters-in-my-bag))
+  (length
+    (adapter-combinations
+      (construct-connectivity-list
+        adapters-in-my-bag))))
 (newline)
