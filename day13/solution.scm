c6d553beca0373d887272fbf1df1fae839fd42ce
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-13 18:17:18 +0200

    Infinite stream implementation for day 13 part 1

--- day13/solution.scm
+++ day13/solution.scm
@@ -26,6 +26,57 @@
     (lambda (p) (> (length p) 0))
     l))
 
+; Stream implementation
+(define (force x)
+  (x))
+
+(define (stream-numbers-from n)
+  (cons
+    n
+    (lambda () (stream-numbers-from (+ n 1)))))
+
+(define (stream-multiples-of n)
+  (stream-map
+    (lambda (x) (* n x))
+    (stream-numbers-from 1)))
+
+(define (stream-constant-of x)
+  (cons
+    x
+    (lambda () (stream-constant-of x))))
+
+(define (stream-take-until predicate stream)
+  (define (loop predicate accumulated rest)
+    (let ((next-value (car rest)))
+      (if (predicate next-value)
+        (reverse (cons next-value accumulated))
+        (loop predicate (cons next-value accumulated) (force (cdr rest))))))
+  (loop predicate '() stream))
+
+(define (stream-map f stream)
+  (cons
+    (f (car stream))
+    (lambda ()
+      (stream-map
+        f
+        (force (cdr stream))))))
+
+(define (stream-zip . streams)
+  (define (stream-zip-two s1 s2)
+    (let ((first (car s1))
+          (second (car s2)))
+      (cons
+        (cons first second)
+        (lambda ()
+          (stream-zip-two
+            (force (cdr s1))
+            (force (cdr s2)))))))
+  (define (loop rest-of-streams result)
+    (if (null? rest-of-streams) result
+      (let ((first-stream (car rest-of-streams)))
+        (loop (cdr rest-of-streams) (stream-zip-two first-stream result)))))
+  (loop streams (stream-constant-of '())))
+
 ; Parser
 (define (parse-input input)
   (let ((lines (omit-empty (split-list-by input '#\newline))))
