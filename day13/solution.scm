fc31ec2f799905b83771b350dc291c51fb74b156
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-13 21:44:10 +0200

    Solution for part 1 of day 13

--- day13/solution.scm
+++ day13/solution.scm
@@ -53,6 +53,27 @@
         (loop predicate (cons next-value accumulated) (force (cdr rest))))))
   (loop predicate '() stream))
 
+(define (stream-find predicate stream)
+  (define (loop predicate rest)
+    (let ((next-value (car rest)))
+      (if (predicate next-value)
+        next-value
+        (loop predicate (force (cdr rest))))))
+  (loop predicate stream))
+
+(define (stream-take stream n)
+  (define (loop rest remaining-n accumulated)
+    (if (<= remaining-n 0) (reverse accumulated)
+      (let ((next-value (car rest)))
+        (loop (force (cdr rest)) (- remaining-n 1) (cons next-value accumulated)))))
+  (loop stream n '()))
+
+(define (stream-drop stream n)
+  (define (loop rest remaining-n)
+    (if (<= remaining-n 0) rest
+        (loop (force (cdr rest)) (- remaining-n 1))))
+  (loop stream n))
+
 (define (stream-map f stream)
   (cons
     (f (car stream))
@@ -75,7 +96,32 @@
     (if (null? rest-of-streams) result
       (let ((first-stream (car rest-of-streams)))
         (loop (cdr rest-of-streams) (stream-zip-two first-stream result)))))
-  (loop streams (stream-constant-of '())))
+  (loop (reverse streams) (stream-constant-of '())))
+
+(define (stream-merge-ordered ordering . streams)
+  (define (stream-merge-two-ordered ordering s1 s2)
+    (let ((first (car s1))
+          (second (car s2)))
+      (if (ordering first second)
+        (cons
+          first
+          (lambda ()
+            (stream-merge-two-ordered
+              ordering
+              (force (cdr s1))
+              s2)))
+        (cons
+          second
+          (lambda ()
+            (stream-merge-two-ordered
+               ordering
+               s1
+               (force (cdr s2))))))))
+  (define (loop rest-of-streams result)
+    (if (null? rest-of-streams) result
+      (let ((first-stream (car rest-of-streams)))
+        (loop (cdr rest-of-streams) (stream-merge-two-ordered ordering first-stream result)))))
+  (loop (cdr streams) (car streams)))
 
 ; Parser
 (define (parse-input input)
@@ -101,10 +147,53 @@
             (list->string id))
           bus-ids)))))
 
+; Solution
+
+(define (bus-schedule-stream bus-ids)
+  (apply stream-merge-ordered
+    (cons
+      (lambda (x y)
+        (< (car x) (car y)))
+      (map
+        (lambda (bus-id)
+          (stream-zip
+            (stream-multiples-of bus-id)
+            (stream-constant-of bus-id)))
+        bus-ids))))
+
+(define (find-first-departure earliest-departure-time bus-schedule-stream)
+  (stream-find
+    (lambda (bus)
+      (>= (car bus) earliest-departure-time))
+    bus-schedule-stream))
+
+(define (answer-to-part-1 earliest-departure-time first-departure)
+  (*
+    (-
+      (car first-departure)
+      earliest-departure-time)
+    (cadr first-departure)))
+
+; Display
+
+(define departure-time-and-bus-ids
+  (parse-input
+    (string->list input-data)))
+
+(define earliest-departure-time
+  (car departure-time-and-bus-ids))
+
+(define bus-ids
+  (cadr departure-time-and-bus-ids))
+
 (newline)
 (display "Part 1:")
 (newline)
 (display
-  (parse-input
-    (string->list input-data)))
+  (answer-to-part-1
+    earliest-departure-time
+    (find-first-departure
+       earliest-departure-time
+       (bus-schedule-stream
+          bus-ids))))
 (newline)
