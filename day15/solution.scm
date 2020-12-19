b02a9626eaf6b50b8712ce6d46b5e4ff534e9618
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-19 23:40:59 +0200

    Solution for part 2 of day 15, still too slow?

--- day15/solution.scm
+++ day15/solution.scm
@@ -4,7 +4,6 @@
 (load "./lib/timings.scm")
 
 (define input-data "0,13,1,16,6,17")
-(define interested-in-index 2020)
 
 ; Parser
 (define (parse-starting-numbers  input)
@@ -16,34 +15,43 @@
       numbers)))
 
 ; Solution
-(define (compute-next-number previous-numbers)
-  (let ((occured-before-index
-          (first-index-of
-            (car previous-numbers)
-            (cdr previous-numbers))))
-    (if occured-before-index
-              (+ 1 occured-before-index)
-              0)))
-
-(define (stream-of-numbers previous-numbers)
-  (let ((next-number (compute-next-number previous-numbers)))
-    (cons
-      next-number
-      (lambda ()
-        (stream-of-numbers
-          (cons next-number previous-numbers))))))
-
 (define (nth-play-number previous-numbers interested-in-nth)
-  (let ((mth-in-stream
-          (-
-            interested-in-nth
-            (length previous-numbers))))
-     (car
-       (reverse
-         (stream-take
-           (stream-of-numbers
-             previous-numbers)
-           mth-in-stream)))))
+  (define (elements-reversed-indexes l)
+    (define (to-hashtable remaining table)
+      (if (null? remaining) table
+        (let ((first (car remaining)))
+          (hash-table-set! table (car first) (cadr first))
+          (to-hashtable (cdr remaining) table))))
+    (let ((list-of-elements-with-indices 
+            (let ((indices (range 1 (length l))))
+              (reverse
+                (zip
+                  (reverse l)
+                  indices)))))
+      (to-hashtable list-of-elements-with-indices (make-strong-eq-hash-table))))
+  ; Indices are 1-based rather than 0-based inside nth-play-number
+  ; to avoid adding/sibtracting one when matching with interested-in-nth
+  (define (loop current-number current-index indices-of-previous)
+    (if (equal? current-index interested-in-nth) current-number
+      (let ((last-seen-current-number
+             (hash-table-ref
+               indices-of-previous
+               current-number
+               (lambda () #f))))
+        (let ((next-number
+                (if last-seen-current-number
+                  (- current-index last-seen-current-number)
+                  0)))
+          (hash-table-set! indices-of-previous current-number current-index)
+          (loop
+            next-number
+            (+ 1 current-index)
+            indices-of-previous)))))
+  (loop
+    (car previous-numbers)
+    (length previous-numbers)
+    (elements-reversed-indexes
+      (cdr previous-numbers))))
 
 ; Display the results
 (define starting-numbers
@@ -58,6 +66,16 @@
 (display
   (with-timings
     (lambda ()
-      (nth-play-number starting-numbers interested-in-index))
+      (nth-play-number starting-numbers 2002))
+    write-timings))
+(newline)
+
+(newline)
+(display "Part 2:")
+(newline)
+(display
+  (with-timings
+    (lambda ()
+      (nth-play-number starting-numbers 30000000))
     write-timings))
 (newline)
