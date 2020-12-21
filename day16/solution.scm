e2441322bb2b88b21e600f56a492bcd7291ee6d2
Anton Ivanov <anton.al.ivanov@gmail.com>
2020-12-21 08:47:06 +0200

    Parsing for the input of day 16

--- day16/solution.scm
+++ day16/solution.scm
@@ -1,4 +1,5 @@
 (load "./lib/list.scm")
+(load "./lib/parser.scm")
 
 (define input-data "class: 1-3 or 5-7
 row: 6-11 or 33-44
@@ -27,17 +28,56 @@ nearby tickets:
           (caddr input-sections))))))
 
 (define (parse-ticket-rules input)
-  input
-)
+  (map
+    parse-ticket-rule
+    input))
+
+(define (parse-ticket-rule input)
+  (define (parse-range input)
+    (let ((parts (split-list-by input '#\-)))
+      (let ((from
+              (string->number
+                (list->string (car parts))))
+            (to
+              (string->number
+                (list->string (cadr parts)))))
+        (make-range from to))))
+  (define (parse-ranges input)
+    (let ((range-inputs
+            (omit-empty
+              (split-list-by-list
+                input
+                (string->list "or")))))
+      (map
+        parse-range
+        range-inputs)))
+  (let ((parts
+          (split-list-by
+            (strip-spaces input)
+            '#\:)))
+    (let ((name
+            (list->string (car parts)))
+          (ranges
+            (parse-ranges (cadr parts))))
+      (make-rule name ranges))))
 
 (define (parse-your-ticket input)
-  input
-)
+  (parse-ticket (cadr input)))
 
 (define (parse-nearby-tickets input)
-  input
-)
+  (map
+    parse-ticket
+    (cdr input)))
 
+(define (parse-ticket input)
+  (let ((numbers (split-list-by input '#\,)))
+    (map
+      (lambda (x)
+        (string->number
+          (list->string x)))
+      numbers)))
+
+; Models
 (define (make-ticket-notes ticket-rules your-ticket nearby-tickets)
   (define (dispatch op)
     (cond ((eq? op 'ticket-rules) ticket-rules)
@@ -47,6 +87,20 @@ nearby tickets:
           (else (error "Unsupported op for ticket-notes:" op))))
   dispatch)
 
+(define (make-rule field-name ranges)
+  (define (dispatch op)
+    (cond ((eq? op 'field-name) field-name)
+          ((eq? op 'ranges) ranges)
+          ((eq? op 'as-list) (list field-name ranges))
+          (else (error "Unsupported op for rule:" op))))
+  dispatch)
+
+(define (make-range from to)
+  (cons from to))
+
+(define (make-ticket field-values)
+  field-values)
+
 ; Display the answers
 
 (define ticket-notes
@@ -55,6 +109,15 @@ nearby tickets:
 
 (newline)
 (display "Part 1:")
+(newline)
+(display
+  (map
+    (lambda (x)
+      (x 'as-list))
+    (ticket-notes 'ticket-rules)))
+(newline)
+(display
+  (ticket-notes 'your-ticket))
 (newline)
 (display
   (ticket-notes 'nearby-tickets))
