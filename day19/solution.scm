1629d3a1665e31b036f071cda79da6dffeeb1ea6
Anton Ivanov <anton.al.ivanov@gmail.com>
2021-01-17 17:04:38 +0200

    Visualize rules and make them more object-oriented for day 19

--- day19/solution.scm
+++ day19/solution.scm
@@ -1,15 +1,83 @@
+(load "./lib/list.scm")
+(load "./lib/parser.scm")
+
+(define input-data "
+0: 1 2
+1: a
+2: 1 3 | 3 1
+3: b
+
+aab
+aba
+bab
+bba
+aaa
+abb
+aabc
+abac
+")
+
+; Parser
+(define (parse-input input)
+  (let ((input-parts
+          (omit-empty
+            (split-list-by
+              (split-list-by
+                input
+                '#\newline)
+                '()))))
+    (cons
+      (parse-rules (car input-parts))
+      (parse-messages (cadr input-parts)))))
+
+(define (parse-rules input)
+  (map
+    parse-rule
+    input))
+
+(define (parse-rule input)
+  (let ((rule-parts
+         (split-list-by
+           input
+           #\space)))
+    ;TODO: parse the rules
+    rule-parts))
+
+(define (parse-messages input)
+  (map
+    list->string
+    input))
+
+(define (parse-instruction input)
+  (let ((input-parts (split-list-by-list input (string->list "=[]"))))
+    (let ((instruction
+            (map
+              list->string
+                (omit-empty
+                  input-parts))))
+      (let ((op-code (car instruction)))
+        (cond ((equal? op-code "mask")
+                 (op-set-mask (cadr instruction)))
+              (else (apply op-memory-write (cdr instruction))))))))
+
+; Rules
 (define (rule-character ch)
-  (lambda (str)
+  (define (match str)
     (cond ((null? str) (cons #f 0))
           ((equal? (car str) ch) (cons #t 1))
-          (else (cons #f 0)))))
+          (else (cons #f 0))))
+  (define (dispatch op)
+    (cond ((eq? op 'match) match)
+          ((eq? op 'as-list) (list "ch:" ch))
+          (else (error "Unsupported op for rule-character:" op))))
+  dispatch)
 
 (define (rule-sequence . rules)
   (define (loop remaining-rules remaining-str offset)
     (if (null? remaining-rules) (cons #t offset)
       (let ((next-rule
               (car remaining-rules)))
-        (let ((next-rule-match (next-rule remaining-str)))
+        (let ((next-rule-match ((next-rule 'match) remaining-str)))
           (let ((next-rule-matches (car next-rule-match))
                 (offset-increment (cdr next-rule-match)))
             (if next-rule-matches
@@ -18,16 +86,25 @@
                 (drop remaining-str offset-increment)
                 (+ offset offset-increment))
               (cons #f offset)))))))
-  (lambda (str)
+  (define (match str)
     (if (null? str) (cons #f 0)
-      (loop rules str 0))))
+      (loop rules str 0)))
+  (define (dispatch op)
+    (cond ((eq? op 'match) match)
+          ((eq? op 'as-list) (list "seq:"
+            (map
+              (lambda (rule)
+                (rule 'as-list))
+              rules)))
+          (else (error "Unsupported op for rule-sequence:" op))))
+  dispatch)
 
 (define (rule-one-of . rules)
   (define (loop remaining-rules str)
     (if (null? remaining-rules) (cons #f 0)
       (let ((next-rule
               (car remaining-rules)))
-        (let ((next-rule-match (next-rule str)))
+        (let ((next-rule-match ((next-rule 'match) str)))
           (let ((next-rule-matches (car next-rule-match))
                 (offset (cdr next-rule-match)))
             (if next-rule-matches
@@ -35,21 +112,39 @@
               (loop
                 (cdr remaining-rules)
                 str)))))))
-  (lambda (str)
+  (define (match str)
     (if (null? str) (cons #f 0)
-      (loop rules str))))
+      (loop rules str)))
+  (define (dispatch op)
+   (cond ((eq? op 'match) match)
+         ((eq? op 'as-list) (list "one-of:"
+           (map
+             (lambda (rule)
+               (rule 'as-list))
+             rules)))
+         (else (error "Unsupported op for rule-one-of:" op))))
+  dispatch)
 
 (define (rule-reference rule-id)
-  (lambda (str)
+  (define (match str)
     (let ((found-rule (assoc rule-id rules)))
       (if found-rule
-        ((cdr found-rule) str)
-        (error "Could not find rule with id" rule-id rules)))))
+        (((cdr found-rule) 'match) str)
+        (error "Could not find rule with id" rule-id rules))))
+  (define (dispatch op)
+   (cond ((eq? op 'match) match)
+         ((eq? op 'as-list) (list "ref:" rule-id))
+         (else (error "Unsupported op for rule-reference:" op))))
+  dispatch)
 
-;0: 1 2
-;1: "a"
-;2: 1 3 | 3 1
-;3: "b"
+; Display the results
+
+;(newline)
+;(display
+;  (parse-input
+;    (string->list input-data)))
+;(newline)
+
 (define rules
   (list
     (cons 0 (rule-sequence
@@ -65,8 +160,6 @@
            (rule-reference 1))))
     (cons 3 (rule-character #\b))))
 
-(define rule-zero
-  (rule-reference 0))
 
 (define input
   (list
@@ -77,9 +170,22 @@
     (string->list "aaa")
     (string->list "abb")))
 
+
+(define rule-zero
+  (rule-reference 0))
+
+;TODO: Also visualize the rules themselves
 (newline)
 (display
   (map
-    rule-zero
+    (rule-zero 'match)
     input))
+(newline)
+
+(newline)
+(display
+  (map
+    (lambda (r)
+      ((cdr r) 'as-list))
+    rules))
 (newline)
